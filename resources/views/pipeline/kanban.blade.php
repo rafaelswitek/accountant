788fc04646d44c17ee09ba8cbf13088519c89a13
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2024-04-29 00:25:28 -0300

    .

--- resources/views/pipeline/kanban.blade.php
+++ resources/views/pipeline/kanban.blade.php
@@ -40,7 +40,7 @@
                                             d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
                                             clip-rule="evenodd" />
                                     </svg>
-                                    <span class="ml-1 leading-none">{{ $deal->created_at }}</span>
+                                    <span class="ml-1 leading-none">{{ $deal->created_at->format('d/m/Y H:i:s') }}</span>
                                 </div>
                                 <div class="relative flex items-center ml-4">
                                     <svg class="relative w-4 h-4 text-gray-300 fill-current"
