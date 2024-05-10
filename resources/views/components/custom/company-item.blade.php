a186ee5950fda20980449b068ac078cc7bccf8b5
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2024-05-09 21:17:51 -0300

    .

---
+++ resources/views/components/custom/company-item.blade.php
@@ -0,0 +1,11 @@
+<li>
+    <div class="flex items-center ps-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+        <input id="{{ $companyDocument }} - {{ $companyName }}" name="dealCompanyId" type="radio"
+            value="{{ $companyId }}"
+            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500 dealCompanyFields"
+            required>
+        <label for="{{ $companyDocument }} - {{ $companyName }}"
+            class="w-full py-2 ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">{{ $companyDocument }}
+            - {{ $companyName }}</label>
+    </div>
+</li>
--- resources/views/pipeline/show.blade.php
+++ resources/views/pipeline/show.blade.php
@@ -35,18 +35,11 @@
                             class="h-20 px-3 pb-3 overflow-y-auto text-sm text-gray-700 dark:text-gray-200"
                             aria-labelledby="dealCompanyId">
                             @foreach ($companies as $company)
-                                <li>
-                                    <div
-                                        class="flex items-center ps-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                                        <input id="{{ $company->document }} - {{ $company->name }}" name="dealCompanyId"
-                                            type="radio" value="{{ $company->id }}"
-                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500 dealCompanyFields"
-                                            required>
-                                        <label for="{{ $company->document }} - {{ $company->name }}"
-                                            class="w-full py-2 ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">{{ $company->document }}
-                                            - {{ $company->name }}</label>
-                                    </div>
-                                </li>
+                                @include('components.custom.company-item', [
+                                    'companyDocument' => $company->document,
+                                    'companyName' => $company->name,
+                                    'companyId' => $company->id,
+                                ])
                             @endforeach
                         </ul>
                         <a href="#"
@@ -197,18 +190,12 @@
                 .then(data => {
                     companiesDiv.textContent = ''
                     data.forEach(element => {
-                        companiesDiv.innerHTML += `
-                            <li>
-                                <div class="flex items-center ps-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                                    <input id="${element.document}-${element.name}" name="dealCompanyId"
-                                        type="radio" value="${element.id}"
-                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500 dealCompanyFields"
-                                        required>
-                                    <label for="${element.document}-${element.name}"
-                                        class="w-full py-2 ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">${element.document} - ${element.name}</label>
-                                </div>
-                            </li>
-                        `;
+                        const html = `@include('components.custom.company-item', [
+                            'companyDocument' => '${element.document}',
+                            'companyName' => '${element.name}',
+                            'companyId' => '${element.id}',
+                        ])`;
+                        companiesDiv.innerHTML += html;
                     });
 
                     attachEventsToDealCompanyFields();
