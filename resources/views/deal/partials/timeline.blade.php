e268e0ea77b2f5a3d096e5465e07a267ca236d37
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2024-05-19 23:50:28 -0300

    .

--- resources/views/deal/partials/timeline.blade.php
+++ resources/views/deal/partials/timeline.blade.php
@@ -111,27 +111,30 @@
                     <li>
                         <a href="#"
                             class="items-center block p-3 sm:flex hover:bg-gray-100 dark:hover:bg-gray-700">
-                            @if (true)
+                            @if (!$row->user->photo)
                                 <div
                                     class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 w-12 h-12 mb-3 me-3 rounded-full sm:mb-0">
                                     <span
                                         class="font-medium text-gray-600 dark:text-gray-300">{{ \App\Helpers\Text::extractInitial($row->user->name) }}</span>
                                 </div>
                             @else
-                                <img class="w-12 h-12 mb-3 me-3 rounded-full sm:mb-0"
-                                    src="https://randomuser.me/api/portraits/women/1.jpg" alt="Jese Leos image" />
+                                <img class="w-12 h-12 mb-3 me-3 rounded-full sm:mb-0" src="{{ $row->user->photo }}"
+                                    alt="{{ $row->user->name }} image" />
                             @endif
                             <div class="text-gray-600 dark:text-gray-400">
                                 <div class="text-base font-normal">{{ $row->title }}</div>
                                 <div class="text-sm font-normal">{{ $row->description }}</div>
                                 <span
                                     class="inline-flex items-center text-xs font-normal text-gray-500 dark:text-gray-400">
-                                    <svg class="w-2.5 h-2.5 me-1" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                        <path
-                                            d="M10 .5a9.5 9.5 0 1 0 0 19 9.5 9.5 0 0 0 0-19ZM8.374 17.4a7.6 7.6 0 0 1-5.9-7.4c0-.83.137-1.655.406-2.441l.239.019a3.887 3.887 0 0 1 2.082 2.5 4.1 4.1 0 0 0 2.441 2.8c1.148.522 1.389 2.007.732 4.522Zm3.6-8.829a.997.997 0 0 0-.027-.225 5.456 5.456 0 0 0-2.811-3.662c-.832-.527-1.347-.854-1.486-1.89a7.584 7.584 0 0 1 8.364 2.47c-1.387.208-2.14 2.237-2.14 3.307a1.187 1.187 0 0 1-1.9 0Zm1.626 8.053-.671-2.013a1.9 1.9 0 0 1 1.771-1.757l2.032.619a7.553 7.553 0 0 1-3.132 3.151Z" />
+                                    <svg class="w-[18px] h-[18px] text-gray-800 dark:text-white" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                        fill="currentColor" viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd"
+                                            d="M5 5a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1 2 2 0 0 1 2 2v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a2 2 0 0 1 2-2ZM3 19v-7a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Zm6.01-6a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm-10 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"
+                                            clip-rule="evenodd" />
                                     </svg>
-                                    Public
+
+                                    {{ \Carbon\Carbon::parse($row->date)->format('d/m/Y H:i') }}
                                 </span>
                             </div>
                         </a>
@@ -156,19 +159,30 @@
                     <li>
                         <a href="#"
                             class="items-center block p-3 sm:flex hover:bg-gray-100 dark:hover:bg-gray-700">
-                            <img class="w-12 h-12 mb-3 me-3 rounded-full sm:mb-0"
-                                src="https://randomuser.me/api/portraits/women/1.jpg" alt="Jese Leos image" />
+                            @if (!$row->user->photo)
+                                <div
+                                    class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 w-12 h-12 mb-3 me-3 rounded-full sm:mb-0">
+                                    <span
+                                        class="font-medium text-gray-600 dark:text-gray-300">{{ \App\Helpers\Text::extractInitial($row->user->name) }}</span>
+                                </div>
+                            @else
+                                <img class="w-12 h-12 mb-3 me-3 rounded-full sm:mb-0" src="{{ $row->user->photo }}"
+                                    alt="{{ $row->user->name }} image" />
+                            @endif
                             <div class="text-gray-600 dark:text-gray-400">
                                 <div class="text-base font-normal">{{ $row->title }}</div>
                                 <div class="text-sm font-normal">{{ $row->description }}</div>
                                 <span
                                     class="inline-flex items-center text-xs font-normal text-gray-500 dark:text-gray-400">
-                                    <svg class="w-2.5 h-2.5 me-1" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                        <path
-                                            d="M10 .5a9.5 9.5 0 1 0 0 19 9.5 9.5 0 0 0 0-19ZM8.374 17.4a7.6 7.6 0 0 1-5.9-7.4c0-.83.137-1.655.406-2.441l.239.019a3.887 3.887 0 0 1 2.082 2.5 4.1 4.1 0 0 0 2.441 2.8c1.148.522 1.389 2.007.732 4.522Zm3.6-8.829a.997.997 0 0 0-.027-.225 5.456 5.456 0 0 0-2.811-3.662c-.832-.527-1.347-.854-1.486-1.89a7.584 7.584 0 0 1 8.364 2.47c-1.387.208-2.14 2.237-2.14 3.307a1.187 1.187 0 0 1-1.9 0Zm1.626 8.053-.671-2.013a1.9 1.9 0 0 1 1.771-1.757l2.032.619a7.553 7.553 0 0 1-3.132 3.151Z" />
+                                    <svg class="w-[18px] h-[18px] text-gray-800 dark:text-white" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                        fill="currentColor" viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd"
+                                            d="M5 5a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1 2 2 0 0 1 2 2v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a2 2 0 0 1 2-2ZM3 19v-7a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Zm6.01-6a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm-10 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"
+                                            clip-rule="evenodd" />
                                     </svg>
-                                    Public
+
+                                    {{ \Carbon\Carbon::parse($row->date)->format('d/m/Y H:i') }}
                                 </span>
                             </div>
                         </a>
