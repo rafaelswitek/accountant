693eaaf67586044e78a6115cd3120ccefd7c36ea
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2024-05-21 22:53:48 -0300

    .

--- resources/views/deal/partials/timeline.blade.php
+++ resources/views/deal/partials/timeline.blade.php
@@ -64,19 +64,13 @@
     <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="default-tab"
         data-tabs-toggle="#default-tab-content" role="tablist">
         <li class="me-2" role="presentation">
-            <button class="inline-block p-4 border-b-2 rounded-t-lg" id="profile-tab" data-tabs-target="#profile"
-                type="button" role="tab" aria-controls="profile" aria-selected="false">Atividades</button>
-        </li>
-        <li class="me-2" role="presentation">
-            <button
-                class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-                id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard"
-                aria-selected="true">Notas</button>
+            <button class="inline-block p-4 border-b-2 rounded-t-lg" id="activity-tab" data-tabs-target="#activity"
+                type="button" role="tab" aria-controls="activity" aria-selected="false">Atividades</button>
         </li>
     </ul>
 </div>
 <div id="default-tab-content">
-    <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="profile" role="tabpanel"
+    <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800 tab-content" id="activity" role="tabpanel"
         aria-labelledby="profile-tab">
         <x-primary-button class="ml-8" x-data=""
             x-on:click.prevent="$dispatch('open-modal', 'activity')">
@@ -123,48 +117,6 @@
             </ol>
         </div>
 
-    </div>
-    <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="dashboard" role="tabpanel"
-        aria-labelledby="dashboard-tab">
-
-        <form>
-            <div
-                class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-                <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
-                    <label for="comment" class="sr-only">Your comment</label>
-                    <textarea id="comment" rows="4"
-                        class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
-                        placeholder="Write a comment..." required></textarea>
-                </div>
-                <div class="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
-                    <button type="submit"
-                        class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
-                        Enviar
-                    </button>
-                </div>
-            </div>
-        </form>
-
-        <div class="w-full mb-4 ">
-            <div
-                class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <a href="#">
-                    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in
-                        Claim?</h5>
-                </a>
-                <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process
-                    on how to certify for your weekly benefits:</p>
-                <a href="#" class="inline-flex font-medium items-center text-blue-600 hover:underline">
-                    See our guideline
-                    <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
-                    </svg>
-                </a>
-            </div>
-
-        </div>
     </div>
 </div>
 
