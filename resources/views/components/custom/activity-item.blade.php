9f40a80c8ba2f96c59af863d163f091f9f9ef1a9
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2024-05-21 22:15:48 -0300

    .

---
+++ resources/views/components/custom/activity-item.blade.php
@@ -0,0 +1,39 @@
+<li class="flex justify-between items-center hover:bg-gray-100 dark:hover:bg-gray-700 draggable" draggable="true"
+    data-activity-id="{{ $activity->id }}">
+    <div class="items-center block p-3 sm:flex">
+        @if (!$activity->user->photo)
+            <div
+                class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 w-12 h-12 mb-3 me-3 rounded-full sm:mb-0">
+                <span
+                    class="font-medium text-gray-600 dark:text-gray-300">{{ \App\Helpers\Text::extractInitial($activity->user->name) }}</span>
+            </div>
+        @else
+            <img class="w-12 h-12 mb-3 me-3 rounded-full sm:mb-0" src="{{ $activity->user->photo }}"
+                alt="{{ $activity->user->name }} image" />
+        @endif
+        <div class="text-gray-600 dark:text-gray-400">
+            <div class="text-base font-normal">{{ $activity->title }}</div>
+            <div class="text-sm font-normal">{{ $activity->description }}</div>
+            <span class="inline-flex items-center text-xs font-normal text-gray-500 dark:text-gray-400">
+                <svg class="w-[18px] h-[18px] text-gray-800 dark:text-white" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                    viewBox="0 0 24 24">
+                    <path fill-rule="evenodd"
+                        d="M5 5a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1 2 2 0 0 1 2 2v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a2 2 0 0 1 2-2ZM3 19v-7a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Zm6.01-6a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm-10 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"
+                        clip-rule="evenodd" />
+                </svg>
+
+                {{ \Carbon\Carbon::parse($activity->date)->format('d/m/Y H:i') }}
+            </span>
+        </div>
+    </div>
+    <div>
+        <x-primary-button class="me-2 remove">
+            <svg class="w-6 h-6 text-white-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+            </svg>
+        </x-primary-button>
+    </div>
+</li>
--- resources/views/deal/partials/timeline.blade.php
+++ resources/views/deal/partials/timeline.blade.php
@@ -1,5 +1,6 @@
 <x-modal name="activity" focusable>
-    <form method="post" action="{{ route('deal.activity.store', ['id' => $deal->id]) }}" class="p-6">
+    <form method="post" action="{{ route('deal.activity.store', ['id' => $deal->id]) }}" class="p-6"
+        onsubmit="disableButton('btnCreateActivity')">
         @csrf
 
         <div class="mt-6">
@@ -53,7 +54,7 @@
                 {{ __('Cancel') }}
             </x-secondary-button>
 
-            <x-primary-button class="ms-3">
+            <x-primary-button class="ms-3" id="btnCreateActivity">
                 {{ __('Create') }}
             </x-primary-button>
         </div>
@@ -64,20 +65,20 @@
         data-tabs-toggle="#default-tab-content" role="tablist">
         <li class="me-2" role="presentation">
             <button class="inline-block p-4 border-b-2 rounded-t-lg" id="profile-tab" data-tabs-target="#profile"
-                type="button" role="tab" aria-controls="profile" aria-selected="false">Atividade</button>
+                type="button" role="tab" aria-controls="profile" aria-selected="false">Atividades</button>
         </li>
         <li class="me-2" role="presentation">
             <button
                 class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
                 id="dashboard-tab" data-tabs-target="#dashboard" type="button" role="tab" aria-controls="dashboard"
-                aria-selected="false">Nota</button>
+                aria-selected="true">Notas</button>
         </li>
     </ul>
 </div>
 <div id="default-tab-content">
     <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="profile" role="tabpanel"
         aria-labelledby="profile-tab">
-        <x-primary-button class="me-2" x-data=""
+        <x-primary-button class="ml-8" x-data=""
             x-on:click.prevent="$dispatch('open-modal', 'activity')">
             <svg class="w-6 h-6 text-white-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                 width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -98,49 +99,8 @@
                 Agendados
             </time>
             <ol class="mt-3 divide-y divider-gray-200 dark:divide-gray-700 overflow-y-scroll h-64 drop-zone">
-                @foreach ($scheduled as $row)
-                    <li class="flex justify-between items-center hover:bg-gray-100 dark:hover:bg-gray-700 draggable"
-                        draggable="true" data-activity-id="{{ $row->id }}">
-                        <div class="items-center block p-3 sm:flex">
-                            @if (!$row->user->photo)
-                                <div
-                                    class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 w-12 h-12 mb-3 me-3 rounded-full sm:mb-0">
-                                    <span
-                                        class="font-medium text-gray-600 dark:text-gray-300">{{ \App\Helpers\Text::extractInitial($row->user->name) }}</span>
-                                </div>
-                            @else
-                                <img class="w-12 h-12 mb-3 me-3 rounded-full sm:mb-0" src="{{ $row->user->photo }}"
-                                    alt="{{ $row->user->name }} image" />
-                            @endif
-                            <div class="text-gray-600 dark:text-gray-400">
-                                <div class="text-base font-normal">{{ $row->title }}</div>
-                                <div class="text-sm font-normal">{{ $row->description }}</div>
-                                <span
-                                    class="inline-flex items-center text-xs font-normal text-gray-500 dark:text-gray-400">
-                                    <svg class="w-[18px] h-[18px] text-gray-800 dark:text-white" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        fill="currentColor" viewBox="0 0 24 24">
-                                        <path fill-rule="evenodd"
-                                            d="M5 5a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1 2 2 0 0 1 2 2v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a2 2 0 0 1 2-2ZM3 19v-7a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Zm6.01-6a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm-10 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-
-                                    {{ \Carbon\Carbon::parse($row->date)->format('d/m/Y H:i') }}
-                                </span>
-                            </div>
-                        </div>
-                        <div>
-                            <x-primary-button class="me-2 remove">
-                                <svg class="w-6 h-6 text-white-800 dark:text-white" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                                    viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
-                                </svg>
-                            </x-primary-button>
-                        </div>
-                    </li>
+                @foreach ($scheduled as $activity)
+                    @include('components.custom.activity-item', ['activity' => $activity])
                 @endforeach
             </ol>
         </div>
@@ -157,49 +117,8 @@
                 Concluidos
             </time>
             <ol class="mt-3 divide-y divider-gray-200 dark:divide-gray-700 overflow-y-scroll h-64 drop-zone">
-                @foreach ($completed as $row)
-                    <li class="flex justify-between items-center hover:bg-gray-100 dark:hover:bg-gray-700 draggable"
-                        draggable="true" data-activity-id="{{ $row->id }}">
-                        <div class="items-center block p-3 sm:flex">
-                            @if (!$row->user->photo)
-                                <div
-                                    class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 w-12 h-12 mb-3 me-3 rounded-full sm:mb-0">
-                                    <span
-                                        class="font-medium text-gray-600 dark:text-gray-300">{{ \App\Helpers\Text::extractInitial($row->user->name) }}</span>
-                                </div>
-                            @else
-                                <img class="w-12 h-12 mb-3 me-3 rounded-full sm:mb-0" src="{{ $row->user->photo }}"
-                                    alt="{{ $row->user->name }} image" />
-                            @endif
-                            <div class="text-gray-600 dark:text-gray-400">
-                                <div class="text-base font-normal">{{ $row->title }}</div>
-                                <div class="text-sm font-normal">{{ $row->description }}</div>
-                                <span
-                                    class="inline-flex items-center text-xs font-normal text-gray-500 dark:text-gray-400">
-                                    <svg class="w-[18px] h-[18px] text-gray-800 dark:text-white" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        fill="currentColor" viewBox="0 0 24 24">
-                                        <path fill-rule="evenodd"
-                                            d="M5 5a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1h1a1 1 0 0 0 1-1 1 1 0 1 1 2 0 1 1 0 0 0 1 1 2 2 0 0 1 2 2v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a2 2 0 0 1 2-2ZM3 19v-7a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Zm6.01-6a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm-10 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0Zm6 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0Zm2 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-
-                                    {{ \Carbon\Carbon::parse($row->date)->format('d/m/Y H:i') }}
-                                </span>
-                            </div>
-                        </div>
-                        <div>
-                            <x-primary-button class="me-2 remove">
-                                <svg class="w-6 h-6 text-white-800 dark:text-white" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                                    viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
-                                </svg>
-                            </x-primary-button>
-                        </div>
-                    </li>
+                @foreach ($completed as $activity)
+                    @include('components.custom.activity-item', ['activity' => $activity])
                 @endforeach
             </ol>
         </div>
@@ -328,7 +247,8 @@
                 method: 'DELETE',
                 headers: {
                     'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute(
+                        'content')
                 },
             };
 
@@ -345,4 +265,8 @@
                 });
         });
     });
+
+    function disableButton(id) {
+        document.getElementById(id).disabled = true;
+    }
 </script>
