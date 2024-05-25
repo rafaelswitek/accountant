8d368b5d8ac3e3097ad77ca6ccc9de16e6543e42
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2024-05-25 16:17:23 -0300

    .

--- resources/views/pipeline/kanban.blade.php
+++ resources/views/pipeline/kanban.blade.php
@@ -23,14 +23,6 @@
                         <a href="/deal/{{ $deal->id }}"
                             class="draggable relative flex flex-col items-start p-4 mt-3 bg-white rounded-lg cursor-pointer bg-opacity-90 group hover:bg-opacity-100"
                             draggable="true" data-deal-id="{{ $deal->id }}" data-deal-name="{{ $deal->name }}">
-                            <button
-                                class="absolute top-0 right-0 flex items-center justify-center hidden w-5 h-5 mt-3 mr-2 text-gray-500 rounded hover:bg-gray-200 hover:text-gray-700 group-hover:flex">
-                                <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                    fill="currentColor">
-                                    <path
-                                        d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-                                </svg>
-                            </button>
                             <span
                                 class="flex items-center h-6 px-3 text-xs font-semibold text-{{ App\Helpers\FromTo::colorStatusDeal($deal->status) }}-500 bg-{{ App\Helpers\FromTo::colorStatusDeal($deal->status) }}-100 rounded-full">{{ App\Helpers\FromTo::statusDeal($deal->status) }}</span>
                             <h4 class="mt-3 text-sm font-medium">{{ $deal->name }}</h4>
