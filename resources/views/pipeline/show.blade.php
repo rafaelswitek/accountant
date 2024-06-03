b544695f02989ee50e578b0c9b64cacbd0a694eb
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2024-06-02 22:20:02 -0300

    .

--- resources/views/pipeline/show.blade.php
+++ resources/views/pipeline/show.blade.php
@@ -103,7 +103,7 @@
 
             <x-primary-button class="me-2" x-data=""
                 x-on:click.prevent="$dispatch('open-modal', 'new-deal')">
-                <svg class="w-6 h-6 text-white-800 dark:text-white" aria-hidden="true"
+                <svg class="w-6 h-6 text-white-800 dark:text-black" aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M18 9V4a1 1 0 0 0-1-1H8.914a1 1 0 0 0-.707.293L4.293 7.207A1 1 0 0 0 4 7.914V20a1 1 0 0 0 1 1h4M9 3v4a1 1 0 0 1-1 1H4m11 6v4m-2-2h4m3 0a5 5 0 1 1-10 0 5 5 0 0 1 10 0Z" />
@@ -112,7 +112,7 @@
             </x-primary-button>
             <a class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 me-2"
                 href="{{ route('pipeline.create') }}">
-                <svg class="w-6 h-6 text-white-800 dark:text-white" aria-hidden="true"
+                <svg class="w-6 h-6 text-white-800 dark:text-black" aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
                     viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
