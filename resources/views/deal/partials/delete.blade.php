5f182c44e6f02c35f6a491f17771c8cfb649a53f
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2024-06-02 23:11:05 -0300

    .

--- resources/views/deal/partials/delete.blade.php
+++ resources/views/deal/partials/delete.blade.php
@@ -24,7 +24,7 @@
 </x-modal>
 <x-primary-button class="px-5 py-2.5 me-2" x-data=""
     x-on:click.prevent="$dispatch('open-modal', 'confirm-deal-deletion')">
-    <svg class="w-6 h-6 text-white-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+    <svg class="w-6 h-6 text-white-800 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
         width="24" height="24" fill="none" viewBox="0 0 24 24">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z">
--- resources/views/deal/partials/timeline.blade.php
+++ resources/views/deal/partials/timeline.blade.php
@@ -74,7 +74,7 @@
         aria-labelledby="profile-tab">
         <x-primary-button class="ml-8" x-data=""
             x-on:click.prevent="$dispatch('open-modal', 'activity')">
-            <svg class="w-6 h-6 text-white-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+            <svg class="w-6 h-6 text-white-800 dark:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                 width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-6 5h6m-6 4h6M10 3v4h4V3h-4Z" />
