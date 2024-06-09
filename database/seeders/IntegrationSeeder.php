2062b1472c288a9760d8e2afd5931a836558aabe
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2024-06-08 22:19:26 -0300

    .

---
+++ database/seeders/IntegrationSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Integration;
+use Illuminate\Database\Seeder;
+
+class IntegrationSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Integration::create([
+            'source' => 'whatsapp',
+            'payload' => [
+                'state' => 'connecting',
+                'qrCode' => '',
+                'user' => [
+                    'name' => '',
+                    'picture' => '',
+                    'status' => '',
+                ],
+                'instanceId' => ''
+            ]
+        ]);
+    }
+}
